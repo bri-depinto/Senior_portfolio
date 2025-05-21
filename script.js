47a89c92d665cd7b884e07168d5c037441493f87
bri.depinto <briannadepinto@gmail.com>
2025-05-21 13:57:16 -0400

    made the form actually work

--- script.js
+++ script.js
@@ -86,6 +86,39 @@ function scrollToSection(id) {
   });
 }
 
+window.onload = function () {
+    document.getElementsByClassName('input-container').textContent = ''
+};
 
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const form = document.getElementById("contactForm");
+    const formData = new FormData(form);
+
+    for (let [name, value] of formData.entries()) {
+        if (value.trim() === "") {
+            alert("Please fill out all fields in order for your message to be sent.");
+            return;
+        }
+    }
+
+    fetch(form.action, {
+        method: "POST",
+        body: formData
+    })
+        .then(response => {
+            if (response.ok) {
+                alert("Message sent!");
+                form.reset();
+            } else {
+                alert("There was a problem sending your message.");
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            alert("There was a problem sending your message.");
+        });
+}
 
 
